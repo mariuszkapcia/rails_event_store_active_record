44db91e18735d56221dc5b85b97835148a66ebc4
Mariusz Kapcia <m.kapcia@nomtek.com>
2016-10-18 16:49:31 +0200

    Casting event type to String

--- lib/rails_event_store_active_record/event.rb
+++ lib/rails_event_store_active_record/event.rb
@@ -6,5 +6,9 @@ module RailsEventStoreActiveRecord
     self.table_name = 'event_store_events'
     serialize :metadata
     serialize :data
+
+    def event_type=(klass)
+      write_attribute(:event_type, klass.to_s)
+    end
   end
 end
